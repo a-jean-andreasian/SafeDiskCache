ae5eff7e1b596bcc91173cb1a79279f2039645c0
a_jean_andreasian <armen_andreasian@proton.me>
2025-10-02 10:35:32 +0700

    Update safe_cache.py

--- safe_cache.py
+++ safe_cache.py
@@ -1,16 +1,19 @@
+import os
 import time
 from typing import Any, Callable, Union
 from diskcache import Cache, Lock, Timeout
-from src.settings import CACHE_DIR
+from settings import CACHE_DIR
 
 DefaultParam = object()
 
 
 class SafeDiskCache:
     @staticmethod
-    def __init__cache():
-        """Initialize a new Cache instance in the root directory with no eviction."""
-        return Cache(directory=CACHE_DIR, eviction_policy='none')
+    def __init__cache(cache_dir):
+        """
+        Initializes a new Cache instance in the root directory with no eviction.
+        """
+        return Cache(directory=cache_dir, eviction_policy='none')
 
     def __retry__(self, retries: int = 3, backoff: float = 0.1, default: Any = False) -> Callable[
         ..., Union[bool, Any]]:
@@ -34,17 +37,15 @@ class SafeDiskCache:
 
                 # Final attempt failed: reinitialize cache
                 self.cache.close()
-                self.cache = self.__init__cache()
+                self.cache = self.__init__cache(cache_dir=self.cache_dir)
                 return default
 
             return wrapper
 
         return decorator
 
-    def __init__(self, retries: int, default: Any = None):
+    def __init__(self, retries: int, default: Any = None, cache_dir=None):
         """
-        Initialize SafeDiskCache instance.
-
         :param int retries: Number of attempts before giving up (minimum 2)
         :param default: Default value returned if key not found (default None)
         """
@@ -53,7 +54,8 @@ class SafeDiskCache:
 
         self.retries = retries - 1  # diskcache already does one retry
         self.default = default
-        self.cache = self.__init__cache()
+        self.cache_dir = cache_dir if (cache_dir and os.path.exists(cache_dir)) else CACHE_DIR
+        self.cache = self.__init__cache(cache_dir=self.cache_dir)
         self.lock = Lock(self.cache, 'global_lock')
 
         # Wrap methods with retry decorator after initialization
